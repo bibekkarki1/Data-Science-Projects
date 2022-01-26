67c8e629732fec3d11ddd4df47419a1a83a895ef
Bibek Karki <93454787+bibekkarki1@users.noreply.github.com>
2022-01-25 19:36:54 -0500

    Add files via upload

---
+++ Airbnb_EDA.sql
@@ -0,0 +1,57 @@
+-- AIRBNB LISTINGS (2021/10/18) Exploratory Data Analysis for Ashville, NC
+-- Data Cleaned: listings with missing data excluded
+
+SELECT * FROM airbnb.listings;
+
+-- Lowest rated listing for each property type where the host is a superhost
+SELECT id, property_type, host_acceptance_rate, review_scores_rating,
+MIN(review_scores_rating) OVER (PARTITION BY property_type) AS min_rating
+FROM airbnb.listings
+WHERE host_is_superhost = 't'
+ORDER BY 2;
+
+
+-- Displaying if the overall rating of a listing is higher, lower, or equal to the previous listing
+SELECT id, review_scores_rating, number_of_reviews, room_type,
+LAG(review_scores_rating) OVER (PARTITION BY room_type ORDER BY id) AS prev_rev_score,
+CASE WHEN review_scores_rating > LAG(review_scores_rating) OVER (PARTITION BY room_type ORDER BY id) THEN 'Higher'
+	WHEN review_scores_rating < LAG(review_scores_rating) OVER (PARTITION BY room_type ORDER BY id) THEN 'Lower'
+	WHEN review_scores_rating = LAG(review_scores_rating) OVER (PARTITION BY room_type ORDER BY id) THEN 'Same'
+	END AS rating_range
+FROM airbnb.listings;
+
+-- Most expensive, 2nd most expensive, and the least expensive listing for each room type
+SELECT id, host_id, property_type, room_type, price,
+FIRST_VALUE(id) OVER W AS most_expensive_listing,
+LAST_VALUE(id) OVER W AS least_expensive_listing,
+NTH_VALUE(id, 2) OVER W AS second_most_expensive_listing
+FROM airbnb.listings
+WINDOW W AS (PARTITION BY room_type ORDER BY price DESC
+			RANGE BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING);
+
+-- Categorizing all listings into 3 cleanliness classifications (very clean, clean, dirty)
+SELECT c.id, host_id, price, review_scores_cleanliness,
+CASE WHEN c.BUCKETS = 1 THEN 'Very Clean'
+	WHEN c.BUCKETS = 2 THEN 'Clean'
+    WHEN c.BUCKETS = 3 THEN 'Dirty'
+    END AS cleanliness_category
+FROM (
+SELECT*,
+NTILE(3) OVER (ORDER BY review_scores_cleanliness desc) AS BUCKETS
+FROM airbnb.listings )c;
+
+-- Stored procedure to return host bio/information by calling the listing id
+DELIMITER $$
+CREATE PROCEDURE host_info(id INT)
+BEGIN
+SELECT id, host_id, host_since, host_response_time, host_response_rate,
+	host_acceptance_rate, host_is_superhost, host_identity_verified
+FROM airbnb.listings;
+END$$
+
+DELIMITER ;
+
+
+
+
+
